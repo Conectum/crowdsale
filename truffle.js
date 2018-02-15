ed69f512b689dd554212fca4bd54cb97a28687f0
Martynas Miliauskas <martin.miliauskas@gmail.com>
2018-02-15 19:35:16 +0200

    add test net config

--- truffle.js
+++ truffle.js
@@ -4,8 +4,13 @@ module.exports = {
       host: "localhost",
       port: 8545,
       network_id: "*", // Match any network id
-      gas: 15000000,
-      gasPrice: 20e9
+      gas: 15000000
+    },
+    kovan: {
+      from: '0x00406dd474c5755f4a5af91c5363a1227aa7cf2a',
+      host: "localhost",
+      port: 8545, 
+      network_id: 42
     }
   }
 };
